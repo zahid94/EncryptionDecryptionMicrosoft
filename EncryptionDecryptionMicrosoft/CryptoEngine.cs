f7ce6604bc2e51b533617a60214d0b9bd4b652ed
AQA-PC-01 <AQA-PC-01@AAQA-PC-01>
2020-07-20 12:55:41 +0600

    Add project files.

---
+++ EncryptionDecryptionMicrosoft/CryptoEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EncryptionDecryptionMicrosoft
+{
+    public class CryptoEngine
+    {
+        private static RSACryptoServiceProvider rSA = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+        public CryptoEngine()
+        {
+            _privateKey = rSA.ExportParameters(true);
+            _publicKey = rSA.ExportParameters(false);
+        }
+
+        //public string publickeystring()
+        //{
+        //    string s = "dgn hgdf gfd hgkdfh";
+        //    //var sw = new StringWriter();
+        //    //var xs = new XmlSerializer(typeof(RSAParameters));
+        //    //xs.Serialize(sw,_publicKey);
+        //    return s;
+        //}
+
+        public string encryption(string paintext)
+        {
+            rSA = new RSACryptoServiceProvider();
+            rSA.ImportParameters(_publicKey);
+
+            var data = Encoding.Unicode.GetBytes(paintext);
+            var cypher = rSA.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+        }
+        public string decryption(string cypher)
+        {
+            var dataBytes = Convert.FromBase64String(cypher);
+            rSA.ImportParameters(_privateKey);
+            var paintext = rSA.Decrypt(dataBytes, false);
+            return Encoding.Unicode.GetString(paintext);
+        }
+    }
+}
---
+++ EncryptionDecryptionMicrosoft/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionDecryptionMicrosoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CryptoEngine crypto = new CryptoEngine();
+            //Console.WriteLine($"Public Key: \n {crypto.publickeystring()}\n");
+
+            Console.WriteLine("Enter Your paintext for encryption.");
+            string cypher = string.Empty;
+            var text = Console.ReadLine();
+            if (!text.Equals(string.Empty))
+            {
+                cypher = crypto.encryption(text);
+                Console.WriteLine($"Cypher text:\n {cypher}\n");
+            }
+
+            Console.WriteLine("enter for decryption");
+            Console.ReadLine();
+            var paintext = crypto.decryption(cypher);
+            Console.WriteLine($"paintext : \n {paintext}\n");
+            Console.ReadLine();
+        }
+    }
+}
